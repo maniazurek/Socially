a88e7ce8660c4a14e2c13e909866760302fd02ce
Marianna Zurek <zurekmarianna@gmail.com>
2022-12-09 11:45:22 +0100

    updated styling in feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,11 +5,51 @@ const Feed = () => {
     <div className="container_layout">
       <h3 className="section_heading">Feed</h3>
       <div className="section_container">
-        <div className="section_container-element"></div>
-        <div className="section_container-element"></div>
-        <div className="section_container-element"></div>
-        <div className="section_container-element"></div>
-        <div className="section_container-element"></div>
+        <div className="section_container-element">
+          <div className="element_details-container">
+            <div className="element_details-img"></div>
+            <div className="element_details">
+              <p className="element_details-profile">Dennis Reynolds</p>
+              <p className="element_details-paragraph">2 hrs ago</p>
+            </div>
+          </div>
+        </div>
+        <div className="section_container-element">
+          <div className="element_details-container">
+            <div className="element_details-img"></div>
+            <div className="element_details">
+              <p className="element_details-profile">Dennis Reynolds</p>
+              <p className="element_details-paragraph">2 hrs ago</p>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="section_container-element">
+          <div className="element_details-container">
+            <div className="element_details-img"></div>
+            <div className="element_details">
+              <p className="element_details-profile">Dennis Reynolds</p>
+              <p className="element_details-paragraph">2 hrs ago</p>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="section_container-element">
+          <div className="element_details-container">
+            <div className="element_details-img"></div>
+            <div className="element_details">
+              <p className="element_details-profile">Dennis Reynolds</p>
+              <p className="element_details-paragraph">2 hrs ago</p>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="section_container-element">
+          <div className="element_details-container">
+            <div className="element_details-img"></div>
+            <div className="element_details">
+              <p className="element_details-profile">Dennis Reynolds</p>
+              <p className="element_details-paragraph">2 hrs ago</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
