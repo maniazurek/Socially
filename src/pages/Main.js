afa752c213ba987c624b0a21c882cd33ebfb2bb0
Marianna Zurek <zurekmarianna@gmail.com>
2022-12-09 01:29:27 +0100

    added bottom navigation from mui

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Feed from "../components/Feed";
 import Messages from "../components/Messages";
 import People from "../components/People";
 import Profile from "../components/Profile";
+import {
+  Home,
+  ChatBubbleOutline,
+  AddCircleOutline,
+  GroupsOutlined,
+  AccountCircle,
+} from "@mui/icons-material";
+import { Paper } from "@mui/material";
 
 const Main = () => {
   return (
@@ -12,7 +21,21 @@ const Main = () => {
       <Messages />
       <People />
       <Profile />
-      <BottomNavigation></BottomNavigation>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={5}
+      >
+        <BottomNavigation>
+          <BottomNavigationAction label="Feed" icon={<Home />} />
+          <BottomNavigationAction
+            label="Messages"
+            icon={<ChatBubbleOutline />}
+          />
+          <BottomNavigationAction label="AddPost" icon={<AddCircleOutline />} />
+          <BottomNavigationAction label="People" icon={<GroupsOutlined />} />
+          <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
+        </BottomNavigation>
+      </Paper>
     </>
   );
 };
