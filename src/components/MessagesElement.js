aa9e5fcbd307a71f41f77adbfe044601c0ed8fee
Marianna Zurek <zurekmarianna@gmail.com>
2023-01-08 18:21:49 +0100

    updated styling

--- src/components/MessagesElement.js
+++ src/components/MessagesElement.js
@@ -10,7 +10,10 @@ const MessagesElement = ({
   const lastMessage = messages[messages.length - 1].message;
   return (
     <Link to={`/conversations/${conversationId}`}>
-      <div className="element_details-message" style={{ backgroundColor: "#e1f6f4"}}>
+      <div
+        className="element_details-message"
+        style={{ backgroundColor: "#e1f6f4" }}
+      >
         <div className="element_details-img-border">
           <div
             className="element_details-img__profile"
@@ -19,7 +22,17 @@ const MessagesElement = ({
         </div>
         <div className="element_details">
           <p className="element_details-name">{interlocutor}</p>
-          <p className="element_details-text" style={{ backgroundColor: "transparent", color: "#656565", border: "none", padding: "0"}}>{lastMessage}</p>
+          <p
+            className="element_details-text"
+            style={{
+              backgroundColor: "transparent",
+              color: "#656565",
+              border: "none",
+              padding: "0",
+            }}
+          >
+            {lastMessage}
+          </p>
         </div>
       </div>
     </Link>
